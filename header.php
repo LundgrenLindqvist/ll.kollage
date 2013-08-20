e0f09cce9a7daf3be029bb41b16573559cd9ef08
Aayush Iyer <aayush.iyer@gmail.com>
2013-08-19 19:14:34 -0600

    Added Viewport for Mobile
    Added viewport for basic responsiveness.

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html <?php language_attributes(); ?>>
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php wp_title('&laquo;', true, 'right'); bloginfo('name'); ?></title>
 
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
